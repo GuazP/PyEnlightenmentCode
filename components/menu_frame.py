4e58450d974f220b6e2ae28f0519281e96111d03
guaz <maciej.pawlowski@mixbox.pl>
2019-12-26 15:00:56 +0100

    Menubar cleanup

--- components/menu_frame.py
+++ components/menu_frame.py
@@ -6,35 +6,34 @@ class MenuBar():
     def __init__(self, _root, MainWindow):
         self._root = _root
         self.menubar = Menu(_root)
-        self.filemenu = Menu(_root, tearoff=0) 
-        self.editmenu = Menu(_root, tearoff=0) 
-        self.helpmenu = Menu(_root, tearoff=0) 
-        
-        self.filemenu.add_command(label = "New", 
-                                  command = MainWindow.new_file) 
-        self.filemenu.add_command(label = "Open", 
-                                  command = MainWindow.load_file) 
-        self.filemenu.add_command(label = "Save", 
-                                  command = MainWindow.save_file) 
-        self.filemenu.add_command(label = "Save as ...", 
-                                  command = lambda: MainWindow.save_file(True)) 
-        self.filemenu.add_separator()
-        self.filemenu.add_command(label = "Exit", 
-                                  command = lambda: MainWindow.exit_(self._root)) 
-        self.menubar.add_cascade(label = "File", 
-                                 menu = self.filemenu) 
+        self.filemenu = Menu(_root, tearoff=0)
+        self.editmenu = Menu(_root, tearoff=0)
+        self.toolmenu = Menu(_root, tearoff=0)
+        self.helpmenu = Menu(_root, tearoff=0)
 
-        self.editmenu.add_command(label = "Settings",
-                                  command = MainWindow.configure_settings)
-        self.menubar.add_cascade(label = "Edit", 
-                                 menu = self.editmenu) 
-          
-        self.helpmenu.add_command(label = "About PyEnlightenmentCode", 
-                                        command = lambda: None) 
-        self.helpmenu.add_cascade(label = "Help", 
-                                       menu = lambda: None) 
-        self.menubar.add_cascade(label = "About", 
-                                 menu = self.helpmenu) 
-          
+        #########> FILE MENU <#########
+        self.filemenu.add_command(label = "New", command = MainWindow.new_file) 
+        self.filemenu.add_command(label = "Open", command = MainWindow.load_file) 
+        self.filemenu.add_command(label = "Save", command = MainWindow.save_file) 
+        self.filemenu.add_command(label = "Save as ...", command = lambda: MainWindow.save_file(True)) 
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label = "Exit", command = lambda: MainWindow.exit_(self._root)) 
+        self.menubar.add_cascade(label = "File", menu = self.filemenu) 
+
+        #########> EDIT MENU <#########
+        self.editmenu.add_command(label = "Settings", command = MainWindow.configure_settings)
+        self.menubar.add_cascade(label = "Edit", menu = self.editmenu) 
+
+        #########> TOOL MENU <#########
+        self.toolmenu.add_command(label = "Execute", command = lambda: None) 
+        self.toolmenu.add_cascade(label = "Build binary", menu = lambda: None) 
+        self.menubar.add_cascade(label = "Tools", menu = self.toolmenu) 
+
+        #########> HELP MENU <#########
+        self.helpmenu.add_command(label = "About PyEnlightenmentCode", command = lambda: None) 
+        self.helpmenu.add_cascade(label = "License", menu = lambda: None) 
+        self.menubar.add_cascade(label = "Help", menu = self.helpmenu) 
+
+        #########> CONFIG TO ROOT <#########
         self._root.config(menu = self.menubar) 
           
